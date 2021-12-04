103ca327f6e7641b1e15a9e11ef88ba8d9190cad
xdwhite86 <xdwhite86x@gmail.com>
2021-12-03 19:34:24 -0500

    ongoing

---
+++ C/AdventOfCode/cmake-build-debug/CMakeFiles/AdventOfCode.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AdventOfCode"
+  "AdventOfCode.pdb"
+  "CMakeFiles/AdventOfCode.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AdventOfCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
