06becd6da97de93c517d5b367f9aaedb3c3996a4
Daniel White <white.dan@prominent.com>
2021-12-13 14:12:13 -0500

    master- ongoing

--- CS/AdventOfCode/NetFramework.FetchAocInput/Program.cs
+++ CS/AdventOfCode/NetFramework.FetchAocInput/Program.cs
@@ -4,30 +4,31 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Threading.Channels;
 using Microsoft.Build.BuildEngine;
+//using System.Threading.Channels;
+//using Microsoft.Build.BuildEngine;
 using Microsoft.VisualBasic;
-using NetCore.stdLibCs;
+//using NetCore.stdLibCs;
 //using Project = Microsoft.Build.Evaluation.Project;
 
 namespace FetchAoCInput
 {
     class Program
     {
-        public static readonly string AocFolder = Std.GetUserHomeDir() + Std.Separator +"AdventOfCode";
+        public static readonly string AocFolder = "C:\\users\\jaksl\\" + "AdventOfCode";
 
         public static readonly string CsAocFolder =
-            AocFolder + Std.Separator + "CS" + Std.Separator + "AdventOfCode" + Std.Separator + "2021";
+            AocFolder + "\\CS\\AdventOfCode\\2021";
         public const string AoCUri = "https://adventofcode.com/2021/day/";
         public const string cookieFile = "C:\\Users\\jaksl\\AdventOfCode\\cookies-adventofcode-com.txt";
         public static readonly string curlFmtString = "curl --cookie {0} -X GET https://adventofcode.com/2021/day/{1}/input";
         static void Main(string[] args)
         {
-            DirectoryInfo dir = new(CsAocFolder);
+            DirectoryInfo dir = new DirectoryInfo(CsAocFolder);
             List<DirectoryInfo> projects = dir.GetDirectories().ToList();
             projects = projects.Where(o => o.Name.Length == 5).ToList();
 
-            List<DirectoryInfo> projectsNeedingInput = new();
+            List<DirectoryInfo> projectsNeedingInput = new List<DirectoryInfo>();
             foreach (var p in projects)
             {
                 if (p.Name == "Day12")
@@ -41,7 +42,7 @@ namespace FetchAoCInput
                     catch (Exception e)
                     {
                         File.Create("Input.txt");
-                        inputFile = new ("Input.txt");
+                        inputFile = new FileInfo("Input.txt");
                     }
 
 
@@ -66,18 +67,20 @@ namespace FetchAoCInput
             if (File.Exists(inputDLFile))
                 File.Delete(inputDLFile);
 
-            using StreamWriter sw = new(inputDLFile);
-            
-            sw.Write(input.TrimEnd('\n', '\r'));
-            sw.Flush();
-            ModifyCsProjForInputFile();
+            using (StreamWriter sw = new StreamWriter(inputDLFile))
+            {
+
+                sw.Write(input.TrimEnd('\n', '\r'));
+                sw.Flush();
+                ModifyCsProjForInputFile();
+            }
         }
 
         private static void ModifyCsProjForInputFile()
         {
             Console.WriteLine(Environment.Version);
             
-            DirectoryInfo dir = new(Environment.CurrentDirectory);
+            DirectoryInfo dir = new DirectoryInfo(Environment.CurrentDirectory);
             var csproj = dir.GetFiles("*.csproj").First();
             
             var projFile = File.ReadAllText(csproj.FullName);
@@ -102,30 +105,32 @@ namespace FetchAoCInput
         {
             string stdOut = string.Empty;
             string stdErr = string.Empty;
-            using Process proc = new()
+            using (Process proc = new Process()
+                   {
+                       StartInfo = new ProcessStartInfo()
+                       {
+                           FileName = "cmd.exe",
+                           Arguments = "/c ",
+                           CreateNoWindow = true,
+                           UseShellExecute = false,
+                           RedirectStandardOutput = true,
+                           RedirectStandardError = true,
+                       },
+                   })
             {
-                StartInfo = new()
-                {
-                    FileName = "cmd.exe",
-                    Arguments = "/c ",
-                    CreateNoWindow = true,
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                },
-            };
-            proc.OutputDataReceived += (sender, eventArgs) => stdOut += eventArgs.Data + "\n";
-            proc.ErrorDataReceived += (sender, eventArgs) => stdErr += eventArgs.Data + "\n";
+                proc.OutputDataReceived += (sender, eventArgs) => stdOut += eventArgs.Data + "\n";
+                proc.ErrorDataReceived += (sender, eventArgs) => stdErr += eventArgs.Data + "\n";
 
-            proc.StartInfo.Arguments += GetCurlString(day);
+                proc.StartInfo.Arguments += GetCurlString(day);
                 //"curl --cookie C:\\Users\\jaksl\\AdventOfCode\\cookies-adventofcode-com.txt -X GET https://adventofcode.com/2021/day/12/input";
 
-            proc.Start();
-            proc.BeginOutputReadLine();
-            proc.BeginErrorReadLine();
-            proc.WaitForExit();
+                proc.Start();
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                proc.WaitForExit();
 
-            return stdOut;
+                return stdOut;
+            }
         }
     }
 }
